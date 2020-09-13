a454c8072befe22e3f8f87e0fed25120ca7ff18f
ZYZR19 <1803255529@qq.com>
2020-09-13 23:05:03 +0800

    code

--- src/day0912/didiinterview.java
+++ src/day0912/didiinterview.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 import java.util.Scanner;
 
-public class Main {
+public class didiinterview {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
